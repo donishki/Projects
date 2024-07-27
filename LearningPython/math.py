684d8cc4784029bfe511addcd8a10077702d2ae1
donishki <dario@pascoal.org>
2024-07-27 19:03:50 +0200

    Learning variables

--- LearningPython/math.py
+++ LearningPython/math.py
@@ -1,1 +1,3 @@
 print(20 * 24 * 60)
+print("This does " + str(69) + " the same thing")
+print(f"20 days are not {60.5} days")
---
+++ LearningPython/variables.py
@@ -0,0 +1,4 @@
+math = 24 * 60 * 60
+units = "seconds"
+
+print(f" 20 days are {20 * math} {units} ")
